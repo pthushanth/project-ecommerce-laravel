75c69b62325d25d15cb4d19b10e524cafcf77aaa
pthushanth <pthushanth97@gmail.com>
2021-03-28 01:37:16 +0100

    product page sale product display

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -136,8 +136,8 @@ class Product extends Model
     public function printPrice()
     {
         if ($this->productSale != null) {
-            return '<p class="prix">' . $this->productSale->getDiscountedPrice($this->getRawOriginal('price')) . ' € <span> ' . $this->price . ' €</span></p>';
-            return $this->productSale->getDiscountedPrice($this->getRawOriginal('price')) . ' €';
+            return $this->productSale->getDiscountedPrice($this->getRawOriginal('price')) . ' <span> ' . $this->price . ' €</span>';
+            // return $this->productSale->getDiscountedPrice($this->getRawOriginal('price')) . ' €';
         } else return $this->price . ' €';
     }
     public function isOnSale()
